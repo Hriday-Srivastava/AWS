f8eefb69d0120f2b5e746d881412fbc4999eb8bb
Hriday-Srivastava <66544564+Hriday-Srivastava@users.noreply.github.com>
2026-01-14 22:51:01 +0530

    Delete k8scruddemo directory

--- k8scruddemo/src/main/java/com/example/k8scruddemo/entity/StudentRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.k8scruddemo.entity;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student, Integer> {
-}
