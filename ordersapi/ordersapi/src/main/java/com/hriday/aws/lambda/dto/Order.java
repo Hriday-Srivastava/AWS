05cd6e6c2097835bcaef490d2b96bcd815bb2dbd
Hriday-Kumar <heartbihar@gmail.com>
2026-03-04 23:11:04 +0530

    Without Dynamo db Create and Get Order lambda function

---
+++ ordersapi/ordersapi/src/main/java/com/hriday/aws/lambda/dto/Order.java
@@ -0,0 +1,23 @@
+package com.hriday.aws.lambda.dto;
+
+public class Order {
+	
+	public int id;
+	public String itemName;
+	public int quantity;
+	
+	public Order() {
+		
+	}
+	
+	public Order(int id, String itemName, int quantity) {
+		super();
+		this.id = id;
+		this.itemName = itemName;
+		this.quantity = quantity;
+	}
+	
+	
+	
+
+}
